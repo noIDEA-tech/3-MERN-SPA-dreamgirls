64eca2a39d7e0f9b2646d7518e934d5efcb654d4
Nancy Watreas <nwatreas2023@gmail.com>
2025-03-20 16:11:40 -0500

    add ESLint configuration, TypeScript settings, and Vite assets for client and server

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,18 @@
+// TODO: review and modify copied file from 18-MERN mini-project below to fit our application.
+module.exports = {
+    env: { browser: true, es2020: true },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react/jsx-runtime',
+      'plugin:react-hooks/recommended',
+    ],
+    parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    settings: { react: { version: '18.2' } },
+    plugins: ['react-refresh'],
+    rules: {
+      'react-refresh/only-export-components': 'warn',
+      'react/prop-types': 'off',
+    },
+  }
+  
